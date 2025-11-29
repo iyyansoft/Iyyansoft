c0879b681f72986acc41a7bce41e8676dc9221bb
Priya-06-12 <priyaangel61003@gmail.com>
2025-11-29 14:40:57 +0530

    fix contact rener url

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -30,7 +30,7 @@ const Contact = () => {
   e.preventDefault();
 
   try {
-    const response = await fetch("http://localhost:5000/send-email", {
+    const response =await fetch(`${import.meta.env.VITE_API_URL}/send-email`,{
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formState),
